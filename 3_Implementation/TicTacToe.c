cded7f64775322ab649048376d0f92e1923aa7e9
naveenrathod <46956641+naveenrathod@users.noreply.github.com>
2022-02-12 18:27:52 +0530

    Add files via upload

--- 3_Implementation/TicTacToe.c
+++ 3_Implementation/TicTacToe.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
-#include <conio.h>
-#include <stdlib.h>
+#include "TicTacToe.h"
+
 
 int Winp();//For checking the winner
 void RowC();
---
+++ 3_Implementation/TicTacToe.h
@@ -0,0 +1,13 @@
+#ifndef TICTACTOE_H_
+#define TICTACTOE_H_
+
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+
+int Winp();
+void RowC();
+
+
+#endif
