3fd227eef1e40080ac22d89787080ad267b61816
Naveen Rathod <naveenbrathpd56@gmail.com>
2022-03-04 16:09:38 +0530

    Added

--- 3_Implementation/project_main.c
+++ 3_Implementation/project_main.c
@@ -1,6 +1,6 @@
 #include "TicTacToe.h"
 
-char space[9] = {'1','2','3','4','5','6','7','8','9'};
+char space[10] = {'o','1','2','3','4','5','6','7','8','9'};
 
 int main(){
 	char xomark; // To mark X,O in the space
